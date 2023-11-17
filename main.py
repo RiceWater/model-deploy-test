3d442587a92ee07e2af6fbe10fefece734009506
RiceWater <chuawillian11@gmail.com>
2023-11-17 15:27:07 +0800

    Add test files

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from prediction import predict_image, preprocess_image, read_image
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home():
+    return {"Message" : "Hello."} 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):   
+    image = read_image(await file.read())
+    image = preprocess_image(image)
+    img_class = predict_image(image)
+    return {"Class": img_class}
---
+++ prediction.py
@@ -0,0 +1,25 @@
+# from json import load
+from PIL import Image
+from io import BytesIO
+
+import numpy as np
+import tensorflow as tf
+
+INPUT_SHAPE = (256,256)
+MODEL = tf.keras.models.load_model('models/sample-thesis-dataset-v3_simple_BN-DO.keras')
+
+def read_image(image_encoded):
+    pil_image = Image.open(BytesIO(image_encoded)).convert('RGB')
+    return pil_image
+
+def preprocess_image(image: Image.Image):
+    resize = tf.image.resize(image, INPUT_SHAPE)
+    ready_image = np.expand_dims(resize/255, 0)
+    return ready_image
+
+def predict_image(image: np.ndarray):
+    y = MODEL.predict(image)
+    y = np.argmax(y)
+    img_class = "Road" if y == 3 else "Not Road"
+    return img_class
+
